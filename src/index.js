3e4c9271dbf445d8754af7f50f5f56d4ff2021f8
va-z <vasilizadorojnyuk@gmail.com>
2020-09-21 14:25:04 +0500

    feat: add number parsing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 function eval() {
-    // Do not use eval!!!
-    return;
+  // Do not use eval!!!
+  return;
 }
 
+// oper - operator array
+// operSt - operator stack
+// operPrec - operator precedences
+// outQ - output queue
+
 function expressionCalculator(expr) {
-    // write your solution here
+  const onTop = (arr) => arr[arr.length - 1];
+  const oper = ["*", "/", "+", "-"],
+    operPrec = {
+      "*": 2,
+      "/": 2,
+      "+": 1,
+      "-": 1,
+    },
+    operSt = [],
+    outQ = [];
+  let buffer = "";
+
+  for (let c of expr) {
+    if (/\d/.test(c)) buffer += c;
+    else if (buffer) {
+      outQ.push(buffer);
+      buffer = "";
+    }
+  }
+  if (buffer) outQ.push(buffer);
 }
 
 module.exports = {
-    expressionCalculator
-}
+  expressionCalculator,
+};
